6032b52911ebb05f498c572ee6e591af24887964
Macedo <108029211+Macedopy@users.noreply.github.com>
2024-01-10 14:02:32 -0300

    Add files via upload

---
+++ Data/SistemaTarefasDBContex.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Controllers;
+using SistemaDeTarefas.Data.Map;
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Data
+{
+    public class SistemaTarefasDBContex : DbContext
+    {
+        public SistemaTarefasDBContex() 
+        {
+
+        }
+        public SistemaTarefasDBContex(DbContextOptions<SistemaTarefasDBContex> options) : base (options)
+        {
+
+        }
+
+        public DbSet<UsuarioModel> Usuarios {get;set;}
+        public DbSet<TarefaModel> Tarefas {get;set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+            modelBuilder.ApplyConfiguration(new TarefaMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
