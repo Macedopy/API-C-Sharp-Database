8d7c1a81a4669eb351533b161798835a90e388fc
Macedo <108029211+Macedopy@users.noreply.github.com>
2024-01-30 22:33:49 -0300

    Add files via upload

---
+++ Data/PaymentAPIContext.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PaymentAPI.Data.Map;
+using PaymentAPI.Models;
+
+namespace PaymentAPI.Data
+{
+    public class PaymentAPIContext : DbContext
+    {
+        public PaymentAPIContext(DbContextOptions<PaymentAPIContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserModel> Users {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
